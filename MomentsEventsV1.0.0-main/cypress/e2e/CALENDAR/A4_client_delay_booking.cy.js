4a0d3107e0f34d73acf28c55f379e05c23f4a009
Guillaume Donni <guillaume.donni@etu.hesge.ch>
2024-04-15 16:08:17 +0200

    ajout A0 creation Arthur

---
+++ MomentsEventsV1.0.0-main/cypress/e2e/CALENDAR/A4_client_delay_booking.cy.js
@@ -0,0 +1,29 @@
+describe('Photographer Availability Scenario', () => {
+  it('allows Julian to set his availability', () => {
+      // Étape 1: Visite de la page d'accueil
+      cy.visit('http://localhost:3000')
+      cy.wait(10) // Attente pour voir la page d'accueil
+
+      // Étape 2: Clique sur le bouton pour se connecter
+      cy.contains('Se connecter').click()
+      cy.wait(10) // Attente pour observer le formulaire de connexion
+
+      // Étape 3: Julian entre ses informations de connexion
+      cy.get('input[id=":r1:"]').type('amelie@example.com')
+      cy.get('input[id=":r3:"]').type('passwordDeJulian123!')
+      cy.wait(10) // Attente après avoir saisi les informations de connexion
+
+      // Étape 4: Julian soumet le formulaire de connexion
+      cy.contains('Me connecter').click()
+      cy.wait(10) // Attente pour vérifier la connexion
+
+      // Étape 5: Vérifier que Julian est redirigé vers la page d'accueil
+      cy.url().should('eq', 'http://localhost:3000/')
+      cy.wait(10) // Attente après la redirection
+
+      // Étape 6: Vérifier que le bouton "Se connecter" est devenu "Mon profil"
+      cy.contains('Mon profil').should('be.visible')
+      cy.wait(10) // Attente pour observer le changement de bouton
+
+  });
+});

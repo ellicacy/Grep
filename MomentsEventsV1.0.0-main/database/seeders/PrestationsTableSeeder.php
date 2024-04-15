06df34b05c9c332ca9566c32138c0b3867283d80
Théo Hurlimann <theo.hurlimann@etu.hesge.ch>
2024-04-15 15:44:45 +0200

    Update : Ajout seeder User et prestation

---
+++ MomentsEventsV1.0.0-main/database/seeders/PrestationsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PrestationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sql = file_get_contents(database_path('sql/prestations/commandesInsert.sql'));
+
+        DB::unprepared($sql);
+    }
+}
